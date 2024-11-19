17d3fa1f92258e2d5daeb4f51a4f7ac6c320312b
Matrix-Mesh <118289247+Matrix-Mesh@users.noreply.github.com>
2024-11-19 14:33:39 +0530

    Corrected the file structure

--- lib/widgets/home_widgets/service_card_content.dart
+++ lib/widgets/home_widgets/service_card_content.dart
@@ -1,26 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:s360/widgets/home_widgets/emergency.dart'; // Import Emergency widget
 import 'package:s360/widgets/home_widgets/service_cards/bus_service_card.dart';
+import 'package:s360/widgets/home_widgets/service_cards/hospital_service_card.dart'; // Import HospitalServiceCard
 import 'package:s360/widgets/home_widgets/service_cards/metro_service_card.dart';
 import 'package:s360/widgets/home_widgets/service_cards/pharmacy_service_card.dart';
 import 'package:s360/widgets/home_widgets/service_cards/police_service_card.dart'; // Import PoliceServiceCard
-import 'package:s360/widgets/home_widgets/service_cards/hospital_service_card.dart'; // Import HospitalServiceCard
-import 'package:url_launcher/url_launcher.dart';
 
 class HomeBodyContent extends StatelessWidget {
   const HomeBodyContent({Key? key}) : super(key: key);
-
-  static Future<void> openMap(String location) async {
-    String googleUrl = "https://www.google.com/maps/search/$location";
-    final Uri _url = Uri.parse(googleUrl);
-
-    try {
-      await launchUrl(_url);
-    } catch (e) {
-      Fluttertoast.showToast(msg: "something went wrong!");
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -65,11 +52,11 @@ class HomeBodyContent extends StatelessWidget {
               crossAxisSpacing: 16,
               padding: const EdgeInsets.all(8),
               children: const [
-                PoliceServiceCard(onMapFunction: openMap),
-                HospitalServiceCard(onMapFunction: openMap),
-                BusServiceCard(onMapFunction: openMap),
-                PharmacyServiceCard(onMapFunction: openMap),
-                MetroServiceCard(onMapFunction: openMap)
+                PoliceServiceCard(),
+                HospitalServiceCard(),
+                BusServiceCard(),
+                PharmacyServiceCard(),
+                MetroServiceCard()
               ],
             ),
           ),
